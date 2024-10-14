5d4e0a5f90e8682aba6f5530426dd9f8096e887e
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-10-14 14:32:37 +0500

    Error handlings

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -27,10 +27,20 @@ fn main() {
         // match to check if user has entered the number Or String
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                // If user didn't add number
+                println!("{}", "Please enter a valid number!".yellow());
+                continue;
+            }
         };
 
         println!("You've guessed {}", guess);
+
+        if guess < low || guess > high {
+            println!("{}", format!("Please enter a number between {} and {}!", low, high).yellow());
+            continue;
+        }
+
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("{}", "Oops, You've guessed smaller number".red()),
@@ -40,6 +50,26 @@ fn main() {
                 break;
             }
             Ordering::Greater => println!("{}", "Oops, You've guessed bigger number".red()),
+        }
+
+
+        let mut try_again = String::new();
+        println!("Do wanna try again? (y/n)");
+
+        io::stdin()
+            .read_line(&mut try_again)
+            .expect("Failed to read input");
+
+        match try_again.trim().to_lowercase().as_str() {
+            "y" => continue,
+            "n" => {
+                println!("Thanks for playing!");
+                break;
+            },
+            _ => {
+                println!("{}", "Invalid input! Exiting the game.".yellow());
+                break;   
+            }
         }
     }
 }
